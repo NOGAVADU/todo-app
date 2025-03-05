c540e3245ce141675e0dade640731125c2877472
Daniil Mosolov <dkuets@gmail.com>
2025-03-05 23:03:53 +0300

    feat: добавил интерфейсы для реализации "чистой архитектуры"

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"github.com/nogavadu/todo-app"
 	"github.com/nogavadu/todo-app/pkg/handler"
+	"github.com/nogavadu/todo-app/pkg/repository"
+	"github.com/nogavadu/todo-app/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewService(services)
 	srv := new(todo.Sever)
 
 	if err := srv.Start("8000", handlers.InitRoutes()); err != nil {
