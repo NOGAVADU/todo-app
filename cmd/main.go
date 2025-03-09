9e4e7f0d57330ec75eed034e9db2810a85637b5f
Daniil Mosolov <dkuets@gmail.com>
2025-03-09 16:29:57 +0300

    feat: добавил файл конфигурации, установил и инициализировал viper

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,28 @@ import (
 	"github.com/nogavadu/todo-app/pkg/handler"
 	"github.com/nogavadu/todo-app/pkg/repository"
 	"github.com/nogavadu/todo-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("failed to initialize config: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewService(services)
 	srv := new(todo.Sever)
 
-	if err := srv.Start("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Start(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("failed to start server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
